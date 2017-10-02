85c1b89174d69022c909d879a7b9f744a74a91f3
truongtvd <truong@smartmove.vn>
2017-10-02 14:04:10 +0700

    init

---
+++ Model/Person.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: truongtvd
+ * Date: 10/2/17
+ * Time: 10:06 AM
+ */
+
+//namespace Model;
+
+
+class Person
+{
+    public $name = "TruongTVD";
+
+}
---
+++ Model/User.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: truongtvd
+ * Date: 10/2/17
+ * Time: 10:01 AM
+ */
+
+class User
+{
+    public $name = "Truongtvd";
+}
